190c729145984031a24bf9a5707081a4f38c8828
teunw <teun.willems+github@live.nl>
2018-04-24 15:03:51 +0200

    Added controllers and stuff

--- src/main/kotlin/com/pts6/politie/controller/StolenController.kt
+++ src/main/kotlin/com/pts6/politie/controller/StolenController.kt
@@ -1,9 +1,10 @@
 package com.pts6.politie.controller
 
+import com.pts6.politie.domain.StolenVehicle
 import com.pts6.politie.service.StolenVehicleService
 import javax.inject.Inject
-import javax.ws.rs.GET
-import javax.ws.rs.Path
+import javax.ws.rs.*
+import javax.ws.rs.core.MediaType.APPLICATION_JSON
 import javax.ws.rs.core.Response
 
 @Path("vehicles/stolen")
@@ -13,8 +14,55 @@ class StolenController {
     private lateinit var stolenVehicleService: StolenVehicleService
 
     @GET
+    @Produces(APPLICATION_JSON)
     fun getStolenVehicles(): Response {
         return Response.ok(stolenVehicleService.getAllStolenVehicles()).build()
+    }
+
+    @GET
+    @Path("/{vehicleId}")
+    @Produces(APPLICATION_JSON)
+    fun getStolenVehicleById(id: Long): Response {
+        return Response.ok(this.stolenVehicleService.getStolenVehicleById(id)).build()
+    }
+
+    @POST
+    @Consumes(APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
+    fun insertStolenVehicle(stolenVehicle: StolenVehicle): Response {
+        return try {
+            this.stolenVehicleService.insertStolenVehicle(stolenVehicle)
+            Response.ok().build()
+        } catch (e: Exception) {
+            Response.serverError().entity(e).build()
+        }
+    }
+
+    @DELETE
+    @Produces(APPLICATION_JSON)
+    fun deleteVehicle(@PathParam("id") id: Long): Response {
+        this.stolenVehicleService.deleteStolenVehicle(id)
+        return Response.ok().build()
+    }
+
+    @PUT
+    @Consumes(APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
+    fun updateStolenVehicle(stolenVehicle: StolenVehicle): Response {
+        return try {
+            this.stolenVehicleService.updateStolenVehicle(stolenVehicle)
+            Response.ok().build()
+        } catch (e: Exception) {
+            Response.serverError().entity(e).build()
+        }
+    }
+
+    @GET
+    @Path("/search/{query}")
+    @Produces(APPLICATION_JSON)
+    fun getStolenVehicleSearch(@PathParam("query") query: String): Response {
+        val vehicles = this.stolenVehicleService.searchStolenVehicles(query)
+        return Response.ok(vehicles).build()
     }
 
 }
--- src/main/kotlin/com/pts6/politie/service/StolenVehicleService.kt
+++ src/main/kotlin/com/pts6/politie/service/StolenVehicleService.kt
@@ -1,16 +1,25 @@
 package com.pts6.politie.service
 
 import com.pts6.politie.domain.StolenVehicle
-import io.github.cdimascio.dotenv.Dotenv
+import org.hibernate.search.exception.EmptyQueryException
+import org.hibernate.search.jpa.FullTextEntityManager
+import org.hibernate.search.jpa.Search
 import javax.ejb.Stateless
 import javax.persistence.EntityManager
+import javax.persistence.EntityManagerFactory
 import javax.persistence.PersistenceContext
+import javax.persistence.PersistenceUnit
 
 @Stateless
 class StolenVehicleService {
 
     @PersistenceContext
     private lateinit var entityManager: EntityManager
+
+    @PersistenceUnit
+    private lateinit var entityManagerFactory: EntityManagerFactory
+
+    private var fullTextEntityManager: FullTextEntityManager? = null
 
     fun getAllStolenVehicles(isStolen: Boolean = true): List<StolenVehicle> =
             this.entityManager
@@ -23,7 +32,40 @@ class StolenVehicleService {
         this.entityManager.persist(stolenVehicle)
     }
 
+    fun getStolenVehicleById(id: Long) = this.entityManager.find(StolenVehicle::class.java, id)
+
     fun updateStolenVehicle(stolenVehicle: StolenVehicle) {
         this.entityManager.merge(stolenVehicle)
+    }
+
+    fun deleteStolenVehicle(id: Long) {
+        this.entityManager.remove(this.entityManager.find(StolenVehicle::class.java, id))
+    }
+
+    private fun getFullTextEntityManager(): FullTextEntityManager {
+        if (this.fullTextEntityManager != null) {
+            return this.fullTextEntityManager!!
+        }
+        val searchEntityManager = this.entityManagerFactory.createEntityManager()
+        this.fullTextEntityManager = Search.getFullTextEntityManager(searchEntityManager)
+        this.fullTextEntityManager?.createIndexer()?.startAndWait()
+        return this.fullTextEntityManager!!
+    }
+
+    fun searchStolenVehicles(query: String): List<StolenVehicle> {
+        val profileQueryBuilder = this.getFullTextEntityManager().searchFactory.buildQueryBuilder().forEntity(StolenVehicle::class.java).get()
+        return try {
+            val profileQuery = profileQueryBuilder
+                    .keyword()
+                    .onFields("licensePlate", "hardwareSn", "stolen")
+                    .matching(query)
+                    .createQuery()
+            this.getFullTextEntityManager()
+                    .createFullTextQuery(profileQuery)
+                    .resultList
+                    .filterIsInstance<StolenVehicle>()
+        } catch (e: EmptyQueryException) {
+            emptyList()
+        }
     }
 }
